c1399913dfc8d1d04efb1c21389a1d404ce35334
Thalis Elias <thalliselias@hotmail.com>
2024-10-24 18:31:42 -0300

    cssTailwind - pronto mas com problemas classes sem funcionar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
     extend: {
       backgroundImage: {
         home: "url('/img/bg.png')",
+      },
+      colors: {
+        "black-50": "rgba(0, 0, 0, 0.5)",
       },
     },
   },
